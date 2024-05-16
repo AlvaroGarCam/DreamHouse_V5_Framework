029087ed38778031f2709f469769c88093814c17
Alvaro Garrido <alvgarcam@alu.edu.gva.es>
2024-05-16 22:15:45 +0200

    Me envía el verify email (depuración de bugs con el .ini)

--- utils/mail.inc.php
+++ utils/mail.inc.php
@@ -7,12 +7,13 @@ class mail
 
     public static function send_resend($data)
     {
-
-        $resend = Resend::client('re_4PeeKQHG_EaS8zcysgnDmteNAz66ifgqD');
+        $resend = parse_ini_file(UTILS . "resend.ini");
+        $API_KEY = $resend['MAIL_API_KEY'];
+        $resend = Resend::client($API_KEY);
         try {
             $result = $resend->emails->send([
                 'from' => 'Acme <onboarding@resend.dev>',
-                'to' => $data['to'],
+                'to' => $data['toEmail'],
                 'subject' => $data['inputMatter'],
                 'html' => $data['inputMessage'],
             ]);
@@ -29,12 +30,10 @@ class mail
     {
         switch ($message['type']) {
             case 'validate';
-                $message['to'] = $message['toEmail'];
                 $message['inputMatter'] = 'Email verification';
                 $message['inputMessage'] = "<h2>Email verification.</h2><a href='http://localhost/DreamHouse_V5_Framework/login/verify/$message[token]'>Click here for verify your email.</a>";
                 break;
             case 'recover';
-                $message['to'] = $message['toEmail'];
                 $message['inputMatter'] = 'Recover password';
                 $message['inputMessage'] = "<a href='http://localhost/DreamHouse_V5_Framework/login/recover/$message[token]'>Click here for recover your password.</a>";
                 break;
